652a42806cc1c8f575e60771d7fc4bc86c83ba4c
Ricardo Perez <ricardoperezgalindo@gmail.com>
2020-10-27 23:36:50 -0300

    mudanças pro ze ler

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const csvFilePath='./input.csv';
 const csv = require('csvtojson');
 const fs = require('fs');
-
+// Substituir ; por , no input.csv
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
@@ -16,17 +16,21 @@ csv()
                     return ({
                 name: p["Nome"],
                 price: p["Preço"],
+                // String ou double ?
                 code: p["Código"],
                 description: p["Descrição"],
                 customizations: [
                     {
                         type: 'MULTI_SELECT',
                         title: 'Adicionais',
-                        fields:  p["Adicionais"].split('/').map(code => {
+                        fields:  p["Adicionais"].split('-').map(code => {
                             const add = complementos.find(comp => comp["Código"] === code);
                             return add ? ({
                                 title: add["Nome"],
-                                price: add["Preço"]
+                                price: add["Preço"],
+                                //String ou double ? 
+                                code: add["Código"]
+                                //Adicionar código para os complementos no banco de dados.
                             }) : undefined;
                         }).filter(field => field)
                     }
