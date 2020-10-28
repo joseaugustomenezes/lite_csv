580b57229f3fdef2d69869978f2417ffd6186ed9
José Augusto Menezes <jaugustonmn@gmail.com>
2020-10-27 22:19:02 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const csvFilePath='./input.csv';
+const csv = require('csvtojson');
+const fs = require('fs');
+const axios = require('axios');
+
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    const produtos = jsonObj.filter((e) => e.Tipo === 'Produto');
+    const complementos = jsonObj.filter((e) => e.Tipo === 'Complemento');
+    const sectionsName = [...new Set(produtos.map(produto => produto["Seção"]))];
+    const menu = {
+        sections: sectionsName.map(s => ({
+            name: s,
+            items: produtos.filter(p => p["Seção"] === s).map(p => 
+                {
+                    return ({
+                name: p["Nome"],
+                price: p["Preço"],
+                code: p["Código"],
+                description: p["Descrição"],
+                customizations: [
+                    {
+                        type: 'MULTI_SELECT',
+                        title: 'Adicionais',
+                        fields:  p["Adicionais"].split('/').map(code => {
+                            const add = complementos.find(comp => comp["Código"] === code);
+                            return add ? ({
+                                title: add["Nome"],
+                                price: add["Preço"]
+                            }) : undefined;
+                        }).filter(field => field)
+                    }
+                ]
+            })})
+        }))
+    };
+});
